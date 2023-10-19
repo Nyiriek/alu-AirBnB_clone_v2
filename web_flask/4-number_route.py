9cc1e5349a5c5935d3143d7dc56160511eff76bc
Nyiriek <n.peat@alustudent.com>
2023-10-19 15:15:11 +0200

    Is it a number?

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -45,7 +45,7 @@ def python(text="is cool"):
     return "Python {}".format(text)
 
 
-@app.route("/number/<n>", strict_slashes=False)
+@app.route("/number/<int:n>", strict_slashes=False)
 def number(n):
     """Displays “n is a number” only if n is an integer"""
     return "{} is a number".format(n)
