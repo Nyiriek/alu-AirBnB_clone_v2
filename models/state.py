ccb37ad041fa9b61aa89bcd4a2cca3bf450d0168
Nyiriek <n.peat@alustudent.com>
2023-10-20 17:24:09 +0200

    Update file

--- models/state.py
+++ models/state.py
@@ -1,27 +1,27 @@
 #!/usr/bin/python3
-"""This is the state class"""
-from os import getenv
-from sqlalchemy import String, DateTime, Column, ForeignKey
-from sqlalchemy.orm import relationship
+""" State Module for HBNB project """
 import models
 from models.base_model import BaseModel, Base
+import os
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 from models.city import City
 
 
 class State(BaseModel, Base):
-    """This is the class for State
-    Attributes:
-        name: input name
-    """
-    __tablename__ = 'states'
+    """ State class """
+
+    __tablename__ = "states"
+
     name = Column(String(128), nullable=False)
 
-    if getenv('HBNB_TYPE_STORAGE') == 'db':
-        cities = relationship('City', backref='state',
-                              cascade='all, delete-orphan')
+    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+        cities = relationship('City', backref="state",
+                              cascade="all, delete, delete-orphan")
     else:
         @property
         def cities(self):
-            """Getter attribute in case of file storage"""
+            """ Returns the list of City instances with state_id
+            equals to the current State.id. """
             return [city for city in models.storage.all(City).values()
                     if city.state_id == self.id]
